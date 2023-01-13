457f739a4706fb64f0c67af6faf5769892fa2672
swoosh3d <timur.esaulov.skill@gmail.com>
2023-01-13 21:21:54 +0700

    global changes

--- copyqqwe.py
+++ copyqqwe.py
@@ -1,29 +1,27 @@
 from PIL import Image
-import pathlib
-from pathlib import Path
+
+SOURCE_DIR = (r'C:\Users\s1mple\Pictures\Saved Pictures\special4py/')
 
 run = True
-SOURCE_DIR = (r'C:\Users\s1mple\Pictures\Saved Pictures\special4py/')
-p1 = input("название файла: ")
-myimage = Image.open(SOURCE_DIR + p1)
 
-command = input("и че мне с этим делать?: ")
-if command == "дать инфу":
+def help():
+    print ("""
+    Для подробной информации о фото используйте команду "info"
+    Для сжатия изображения используйте команду "compress"
+    Для изменения размера фото используйте команду "resize"
+    Для создания картинки размером 800x600 используйте команду "preview"
+    Для сжатия и создания превью одного и того же изображения используйте команду "compress and preview"
+    Для завершения программы используйте "exit"
+    """)
+
+def info():
     print(myimage.size)
     print(myimage.mode)
     print(myimage.format)
     print(myimage.info)
     myimage.show()
 
-if command == "help":
-    print ("""
-Для сжатия изображения используйте команду "compress"
-Для изменения размера фото используйте команду "resize"
-Для создания картинки размером 800x600 используйте команду "preview"
-Для сжатия и создания превью одного и того же изображения используйте команду "compress and preview"
-    """)
-
-if command == "resize":
+def resize():
     width0 = int(input("размер в пикселях ширины: "))
     width1 = int(input("размер в пикселях высоты: "))
     newimage = myimage.resize((width0, + width1))
@@ -31,23 +29,46 @@ if command == "resize":
     rgbnewim.save(SOURCE_DIR + "NEWIMG.jpeg")
     rgbnewim.show()
 
-if command == "compress":
+def compress():
     newimage = myimage.quantize(method=2)
     compressedimg = newimage.convert('RGB')
     compressedimg.save(SOURCE_DIR + "COMPRESSED.jpeg")
     compressedimg.show()
 
-if command == "preview":
+def preview():
     preview = myimage.resize((800, 600))
     preview = preview.convert('RGB')
     preview.save(SOURCE_DIR + "PREVIEW.jpeg")
     preview.show()
 
-if command == "compress and preview":
+def comress_and_preview():
     preview = myimage.resize((800, 600))
     preview = preview.convert('RGB')
     preview.save(SOURCE_DIR + "PREVIEW.jpeg")
     compressedimg = myimage.quantize(method=2)
     compressedimg.save(SOURCE_DIR + "COMPRESSED.jpeg")
-    
-    
+        
+while run:
+    file_name = input("название файла: ")
+
+    myimage = Image.open(SOURCE_DIR + file_name)
+
+    command = input("Введите команду: ")
+
+    if command == "info":
+        info()
+    elif command == "help":
+        help()
+    elif command == "resize":
+        resize()
+    elif command == "compress":
+        compress()
+    elif command == "preview":
+        preview()
+    elif command == "compress_and_preview":
+        comress_and_preview()
+    elif command == "exit":
+        print("удачи!")
+        break
+    else:
+        help()
